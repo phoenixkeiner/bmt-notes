d623c5c75e965abbbc33d8af8ef0a242449f1f75
Phoenix <pkeiner@messengerllc.com>
2026-05-12 11:12:47 -0400

    tailwind color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ff6123',
+        'belt-white': '#f4ede7',
+        'belt-yellow': '#c49b1e',
+        'belt-orange': '#ff6123',
+        'belt-blue': '#003366',
+        'belt-purple': '#4b0082',
+        'belt-brown': '#8b4514',
+        'belt-black': '#3b3b3b',
+        'strike-left': '#3b82f6',
+        'strike-right': '#ef4444',
+      },
+    },
   },
   plugins: [],
-}
+};
